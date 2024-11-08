e9ddca9afbd70f5322d7be4ad02e9fd3d7e36a13
Robert Bunch <rdbunch@gmail.com>
2024-11-08 17:52:43 -0500

    Load device and show buttons

--- video-conf/front-end/main.js
+++ video-conf/front-end/main.js
@@ -3,6 +3,8 @@ import buttons from './uiStuff/uiButtons'
 import { io } from 'socket.io-client'
 import { Device } from 'mediasoup-client'
 
+let device = null
+
 // const socket = io.connect('https://localhost:3031')
 //FOR LOCAL ONLY... no https
 const socket = io.connect('http://localhost:3031')
@@ -15,7 +17,13 @@ const joinRoom = async()=>{
   const userName = document.getElementById('username').value
   const roomName = document.getElementById('room-input').value
   const joinRoomResp = await socket.emitWithAck('joinRoom',{userName,roomName})
-  console.log(joinRoomResp)
+  // console.log(joinRoomResp)
+  device = new Device()
+  await device.load({routerRtpCapabilities: joinRoomResp.routerRtpCapabilities})
+  console.log(device)
+  
+  //PLACEHOLDER... start making the transports for current speakers
+  buttons.control.classList.remove('d-none')
 }
 
 buttons.joinRoom.addEventListener('click',joinRoom)
